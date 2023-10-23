d0738945de9b9ecabc0032c5b79baa5e53b54215
Valeria Navarro <valerianavarro1012@gmail.com>
2023-10-23 10:00:25 -0500

    tipado services

--- src/app/core/services/characters.service.ts
+++ src/app/core/services/characters.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http'
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Character, CharacterResults } from '../models/character.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +11,12 @@ export class CharactersService {
 
   constructor(private _http: HttpClient) { }
 
-  getCharacters(limit?:number, offset?: number, orderBy: string='name', searchName?: string){
+  getCharacters(limit?:number, offset?: number, orderBy: string='name', searchName?: string): Observable<Character>{
     const data = searchName ?`&nameStartsWith=${searchName}`:''
-    return this._http.get(`${environment.API}/characters?limit=${limit}&offset=${offset}&orderBy=${orderBy}${data}`)
+    return this._http.get<Character>(`${environment.API}/characters?limit=${limit}&offset=${offset}&orderBy=${orderBy}${data}`)
   }
 
-  getCharacterById(id:string){
-    return this._http.get(`${environment.API}/characters/${id}`)
+  getCharacterById(id:string): Observable<CharacterResults>{
+    return this._http.get<CharacterResults>(`${environment.API}/characters/${id}`)
   }
 }
